d86836b2827422ad1ff41a0bb5a68b51e50eb4c4
imaduddinaf <pentol231094@gmail.com>
2015-11-02 10:29:41 +0700

    Basic gameplay = click n times to destroy

---
+++ Volcannon/Assets/Projectile/Projectile.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour 
+{
+    public int numberOfAngle;
+    private int numberOfClickToDestroy;
+    private int numberOfClick;
+    private RuntimePlatform platform = Application.platform;
+	// Use this for initialization
+	void Start () 
+    {
+        numberOfClickToDestroy = numberOfAngle;
+        numberOfClick = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    // click detection
+        OnClickObject();
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "ProjectileDestroyer")
+        {
+            //Debug.Log("crut" + col.gameObject.tag);
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Debug.Log("cruts");
+    }
+
+    void OnClickObject()
+    {
+        if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer)
+        {
+            if (Input.touchCount > 0)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    CheckTouch(Input.GetTouch(0).position);
+                }
+            }
+        }
+        else if (platform == RuntimePlatform.WindowsEditor)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                CheckTouch(Input.mousePosition);
+            }
+        }
+    }
+
+    void CheckTouch(Vector3 pos)
+    {
+        Vector3 wp = Camera.main.ScreenToWorldPoint(pos);
+        Vector2 touchPosition = new Vector2(wp.x, wp.y);
+        Collider2D hit = Physics2D.OverlapPoint(touchPosition);
+
+        if (hit != null)
+        {
+            numberOfClick++;
+            Debug.Log("click : " + numberOfClick);
+        }
+
+        if (numberOfClick >= numberOfClickToDestroy)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Volcannon/Assets/Projectile/ProjectileSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpawner : MonoBehaviour 
+{
+    // projectile
+    private int projectileIndex;
+    private GameObject[] projectiles;
+    public GameObject square;
+
+    // spawn time
+    private float spawnTime = 5.0f;
+    private float spawnTimeCounter;
+
+	// Use this for initialization
+	void Start () 
+    {
+        // projectile init
+        projectileIndex = 0; // di implementasinya, random
+        projectiles = new GameObject[1];
+        projectiles[0] = square;
+
+        //spawn time init
+        spawnTime = 5.0f;
+        spawnTimeCounter = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Debug.Log("counter : " + spawnTimeCounter);
+        if (spawnTimeCounter > spawnTime)
+        {
+            Debug.Log("spawn");
+            Instantiate(projectiles[0]);
+            spawnTimeCounter = 0.0f;
+        }
+        spawnTimeCounter += Time.deltaTime;
+	}
+}
